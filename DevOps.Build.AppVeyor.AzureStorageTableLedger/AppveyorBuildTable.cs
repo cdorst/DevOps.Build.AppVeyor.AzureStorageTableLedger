d7920f629890ddc34441eaf73e984e24226f92ea
Christopher Dorst <chris@dorst.email>
2018-03-24 15:45:44 -0500

    Add AppveyorBuildTable.cs

---
+++ DevOps.Build.AppVeyor.AzureStorageTableLedger/AppveyorBuildTable.cs
@@ -0,0 +1,23 @@
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace DevOps.Build.AppVeyor.AzureStorageTableLedger
+{
+    /// <summary>Azure Table Storage entity representing a successfully completed AppVeyor build</summary>
+    public class AppveyorBuildTable : TableEntity
+    {
+        public AppveyorBuildTable()
+        {
+        }
+
+        public AppveyorBuildTable(string name, string version, string dependencies = null, string fileHashes = null)
+        {
+            Dependencies = dependencies; FileHashes = fileHashes; PartitionKey = name; RowKey = version;
+        }
+
+        /// <summary>Comma-delimited string of repository dependencies in {name}|{version} format</summary>
+        public string Dependencies { get; set; }
+
+        /// <summary>Comma-delimited string of repository file hashes</summary>
+        public string FileHashes { get; set; }
+    }
+}
