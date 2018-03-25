e56c17e309f72b908432e7e17b5b68d38eb1d71b
Christopher Dorst <chris@dorst.email>
2018-03-25 16:50:42 -0500

    Update AppveyorBuildTable.cs

--- DevOps.Build.AppVeyor.AzureStorageTableLedger/AppveyorBuildTable.cs
+++ DevOps.Build.AppVeyor.AzureStorageTableLedger/AppveyorBuildTable.cs
@@ -1,3 +1,6 @@
+// Copyright © Christopher Dorst. All rights reserved.
+// Licensed under the GNU General Public License, Version 3.0. See the LICENSE document in the repository root for license information.
+
 using Microsoft.WindowsAzure.Storage.Table;
 
 namespace DevOps.Build.AppVeyor.AzureStorageTableLedger
@@ -11,20 +14,20 @@ namespace DevOps.Build.AppVeyor.AzureStorageTableLedger
 
         public AppveyorBuildTable(string name, string version, string dependencies = null, string fileHashes = null)
         {
-Dependencies = dependencies;
-FileHashes = fileHashes;
-PartitionKey = name;
-RowKey = version;;
+            Dependencies = dependencies;
+            FileHashes = fileHashes;
+            PartitionKey = name;
+            RowKey = version;;
         }
 
         /// <summary>Comma-delimited string of repository dependencies in {name}|{version} format</summary>
-public string Dependencies { get;
-set;
-};
+        public string Dependencies { get;
+        set;
+        };
 
         /// <summary>Comma-delimited string of repository file hashes</summary>
-public string FileHashes { get;
-set;
-};
+        public string FileHashes { get;
+        set;
+        };
     }
 }
